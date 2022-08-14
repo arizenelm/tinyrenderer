ca1a64e2ffd421f60b42186670883056ef31c2b7
Egor Grichin <egorgrichin@gmail.com>
2022-08-15 00:36:58 +0700

    model версия 1

--- model.cpp
+++ model.cpp
@@ -1,2 +1,40 @@
 #include "model.h"
+#include <fstream>
+#include <string>
+#include <sstream>
 
+
+void Model::init(std::string filename)
+{
+    std::ifstream input(filename);
+    std::string line;
+    
+
+    char trash, slash;
+    int extra_val;
+    Vec3i vertex;
+
+    while (getline(input, line))
+    {
+        std::istringstream iline(line);
+        iline >> trash;
+
+        if (trash == 'v')
+        {
+            iline >> vertex.x() >> vertex.y() >> vertex.z();
+            verts.push_back(vertex);
+        }
+
+        else if (trash == 'f')
+        {
+            faces.push_back(std::vector<int>(3));
+            for (int i = 0; i < 3; i++)
+            {
+                iline >> (faces.front())[i] >> slash >> extra_val >> slash >> extra_val;
+            }
+        }
+    }
+
+    n_faces = faces.size();
+    n_verts = verts.size();
+}
--- model.h
+++ model.h
@@ -2,7 +2,26 @@
 #define MODEL_H
 
 #include "geometry.h"
+#include <string>
+#include <vector>
 
-void foo(vec2<int>&) {}
+class Model
+{
+private:
+    unsigned int n_faces;
+    unsigned int n_verts;
+    
+public:
+    std::vector<Vec3i> verts;
+    std::vector<std::vector<int>> faces;
+
+    Model() : n_faces(0), n_verts(0) {}
+
+    void init(std::string);
+
+    inline unsigned int nfaces() { return n_faces; }
+    inline unsigned int nverts() { return n_verts; }
+    
+};
 
 #endif
