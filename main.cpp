00ba239d7faaf0278b960433f0896b7fd76f9143
Egor Grichin <egorgrichin@gmail.com>
2022-09-03 00:07:07 +0700

    Revert "light"
    This reverts commit d3b6d583398955065b840dd881e84338031627c5.

--- main.cpp
+++ main.cpp
@@ -37,12 +37,9 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
     int dy = abs(y1 - y0);
     int inc = (y1 - y0) / abs(y1 - y0);
     int dy_2 = 2 * abs(y1 - y0);
+    image.set(y, x, color);   // fix needed? //
     do
     {
-        if (swapped)
-            image.set((int)y, x, color);
-        else
-            image.set(x, (int)y, color);
         x += 1;
         if (dy_2 >= dx_2)
         {
@@ -50,6 +47,10 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
             dx_2 += 2 * dx;
         }
         dy_2 += 2 * dy;
+        if (swapped)
+            image.set((int)y, x, color);
+        else
+            image.set(x, (int)y, color);
     } while (x < x1);
     
 
